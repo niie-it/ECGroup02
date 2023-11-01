5147e49c13c521427c9ba11b595d6143b0c87544
HIENLTH 2023 <hyhien@gmail.com>
2023-11-01 18:10:10 +0700

    Register customer

---
+++ MyCommerce/MyCommerce/Controllers/KhachHangController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCommerce.Data;
+using MyCommerce.Helpers;
+using MyCommerce.Models;
+
+namespace MyCommerce.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly MyeStoreContext _ctx;
+
+        public KhachHangController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DangKy(RegisterVM model, IFormFile FileHinh)
+        {
+            try
+            {
+                var khachHang = new KhachHang
+                {
+                    MaKh = model.MaKh,
+                    HoTen = model.HoTen,
+                    NgaySinh = model.NgaySinh,
+                    DiaChi = model.DiaChi,
+                    GioiTinh = model.GioiTinh,
+                    DienThoai = model.DienThoai,
+                    Email = model.Email,
+                    Hinh = MyTool.UploadFileToFolder(FileHinh, "KhachHang"),
+                    HieuLuc = true,//tùy điều kiện
+                    RandomKey = MyTool.GetRandom()
+                };
+                khachHang.MatKhau = model.MatKhau.ToMd5Hash(khachHang.RandomKey);
+                _ctx.Add(khachHang);
+                _ctx.SaveChanges();
+                return RedirectToAction("DangNhap");
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+
+        }
+    }
+}
